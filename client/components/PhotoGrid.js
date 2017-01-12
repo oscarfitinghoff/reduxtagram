587ebaa4fadabcf3cc54dd78e7e9218357328e77
oscar <oscar.fitinghoff@gmail.com>
2017-01-12 13:55:33 +0100

    Moved to develop

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
   render() {
     return (
       <div className="photo-grid">
-        Grid
+        {this.props.posts.map((post, i) => <Photo />)}
       </div>
-      )
+    )
   }
-})
+});
 
 export default PhotoGrid;
